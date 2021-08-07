3963bb4eaba273b199e4fd80fa259d8f96650df0
louis-manabat <manabat.online@gmail.com>
2021-08-07 12:37:05 +1000

    [Fix/Task] Fixed reference error and added public az1

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "sic_vpc" {
 }
 
 resource "aws_internet_gateway" "sic_igw" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.sic_vpc.id
 
   tags = {
     Name = "sic-igw"
@@ -24,5 +24,16 @@ resource "aws_default_route_table" "sic-drt" {
 
   tags = {
     Name = "sic-drt"
+  }
+}
+
+resource "aws_subnet" "public_az1" {
+  vpc_id                  = aws_vpc.sic_vpc.id
+  cidr_block              = "10.0.0.0/22"
+  availability_zone       = "ap-southeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sic-public-az1"
   }
 }
